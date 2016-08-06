372ce072396be3dfed0fef0427e0ceef8a902e06
TEEMM <temuri.fxaladze02@geolab.edu.ge>
2016-08-07 01:42:18 +0400

    validation built in

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './media-item-list.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './media-item-list.component', './media-item-form.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './media-item-list.component'], function(expor
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, media_item_list_component_1;
+    var core_1, media_item_list_component_1, media_item_form_component_1;
     var AppComponent;
     return {
         setters:[
@@ -19,6 +19,9 @@ System.register(['angular2/core', './media-item-list.component'], function(expor
             },
             function (media_item_list_component_1_1) {
                 media_item_list_component_1 = media_item_list_component_1_1;
+            },
+            function (media_item_form_component_1_1) {
+                media_item_form_component_1 = media_item_form_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -27,7 +30,7 @@ System.register(['angular2/core', './media-item-list.component'], function(expor
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'media-tracker-app',
-                        directives: [media_item_list_component_1.MediaItemListComponent],
+                        directives: [media_item_list_component_1.MediaItemListComponent, media_item_form_component_1.MediaItemFormComponent],
                         templateUrl: 'app/app.component.html',
                         styleUrls: ['app/app.component.css']
                     }), 
